c486349d2f95ffd086af416567e06449030c28fb
Denis Denisov <desigens@gmail.com>
2013-09-07 16:42:20 +0400

    модель съеденного продукта, поиск съеденного продукта в базе

--- app.js
+++ app.js
@@ -21,9 +21,55 @@ var ProductsDBCollection = Backbone.Collection.extend({
 var productsDB = new ProductsDBCollection();
 productsDB.reset(db);
 
-productsDB.each(function (element) {
-	console.log(element.get('name'))
+// productsDB.each(function (element) {
+// 	console.log(element.get('name'))
+// });
+
+var Eaten = Backbone.Model.extend({
+	attributes: {
+		string: "",
+		productModel: ""
+	},
+	initialize: function () {
+		
+	},
+	filterProduct: function () {
+		var string = this.get('string'),
+			db = productsDB,
+			regexp = new RegExp(string);
+
+		var found = productsDB.filter(function(item){
+			name = item.get('name');
+        	return regexp.test(name);
+		});
+
+		return found;
+	},
+	findProduct: function (string) {
+		var found;
+
+		if (string) {
+			this.set('string', string);
+		}
+
+		found = this.filterProduct();
+
+		if (found.length > 1) {
+			this.set('productModel', undefined);
+			return "Нужно уточнить (найдено " + found.length + ")";
+		} else if (found.length === 1) {
+			this.set('productModel', found[0]);
+			return "Найден подходящий продукт!";
+		} else {
+			this.set('productModel', undefined);
+			return "Ничего не найдено";
+		}
+	}
 });
+
+var e = new Eaten();
+
+console.log(e.findProduct('о'));
 
 
 //TODO рассчет съеденного
