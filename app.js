b58d58a9dbc4c4ba0b0076d4a7f5bde90d7ffeb8
Denis Denisov <desigens@gmail.com>
2013-09-07 23:49:06 +0400

    Корректировка JSON продуктов, метод рассчета съеденного продукта

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var ProductModel = Backbone.Model.extend({
 		carbohydrates: NaN, //на 100гр
 		fats: NaN, //на 100гр
 		calories: NaN, //на 100гр
-		
 		name: undefined,
 		portion: NaN // Вес порции (штуки, пачки)
 	}
@@ -21,22 +20,51 @@ var ProductsDBCollection = Backbone.Collection.extend({
 var productsDB = new ProductsDBCollection();
 productsDB.reset(db);
 
-// productsDB.each(function (element) {
-// 	console.log(element.get('name'))
-// });
-
 var Eaten = Backbone.Model.extend({
 	attributes: {
 		string: "",
 		productModel: ""
 	},
 	initialize: function () {
-		
+		this.on('change', this.calcNutrion, this);
+		this.on('change:string', this.findProduct, this);
+		this.on('change:productModel', this.showMatch, this)
+	},
+	showMatch: function () {
+		var product = this.get('productModel'),
+			text;
+
+		if (product) {
+			text = 'Найден подходящий продукт! ';
+			text += product.get('name') + ' (';
+			text += 'белки: ' + product.get('proteins');
+			text += ', углеводы: ' + product.get('carbohydrates');
+			text += ', жиры: ' + product.get('fats');
+			text += ', калории: ' + product.get('calories');
+			text += ")";
+			console.log(text);
+		}
+	},
+	calcNutrion: function () {
+		var product = this.get('productModel'),
+			weight = parseInt(this.get('weight')) || 100,
+			text;
+
+		if (product) {
+			text = "Съедено: ";
+			text += 'белков: ' + (parseInt(product.get('proteins')) * weight / 100);
+			text += ', углеводов: ' + (parseInt(product.get('carbohydrates')) * weight / 100);
+			text += ', жиров: ' + (parseInt(product.get('fats')) * weight / 100);
+			text += ', калорий: ' + (parseInt(product.get('calories')) * weight / 100);
+			console.log(text);
+		}	
 	},
 	filterProduct: function () {
 		var string = this.get('string'),
 			db = productsDB,
 			regexp = new RegExp(string);
+
+			// console.log('OLOLO', string, regexp);	
 
 		var found = productsDB.filter(function(item){
 			name = item.get('name');
@@ -45,12 +73,9 @@ var Eaten = Backbone.Model.extend({
 
 		return found;
 	},
-	findProduct: function (string) {
-		var found;
-
-		if (string) {
-			this.set('string', string);
-		}
+	findProduct: function () {
+		var string = this.get('string'),
+			found;
 
 		found = this.filterProduct();
 
@@ -59,17 +84,17 @@ var Eaten = Backbone.Model.extend({
 			return "Нужно уточнить (найдено " + found.length + ")";
 		} else if (found.length === 1) {
 			this.set('productModel', found[0]);
-			return "Найден подходящий продукт!";
+			return;
 		} else {
 			this.set('productModel', undefined);
 			return "Ничего не найдено";
 		}
-	}
+	},
+
 });
 
 var e = new Eaten();
-
-console.log(e.findProduct('о'));
-
-
-//TODO рассчет съеденного
+e.set({
+	'string': 'тун',
+	'weight': 40
+});
